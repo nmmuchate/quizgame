63be9009944b95e69c6b28c39fe140c6203d5baf
nmmuchate <nicolas.muchate@gmail.com>
2020-04-05 11:21:54 +0200

    atualização de codigo.

---
+++ Assets/Scripts/comandosBasicos.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class comandosBasicos : MonoBehaviour {
+
+	public void carregaCena(string nomeCena)
+	{
+		Application.LoadLevel(nomeCena);
+	}
+}
---
+++ Assets/Scripts/moveOffSet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveOffSet : MonoBehaviour {
+
+	private Material materialAtual;
+	public float velocidade;
+	private float offset;
+
+
+
+	// Use this for initialization
+	void Start () {
+		materialAtual = GetComponent<Renderer>().material;
+
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		offset += 0.25f;
+
+		materialAtual.SetTextureOffset("_MainTex", new Vector2(offset * velocidade, 0));
+
+	}
+}
